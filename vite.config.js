c2ca4b02e0e5658734281c6e1deae7fbe12a2a12
Thinhncse <cuongthinh5213@gmail.com>
2025-01-16 02:27:39 +0700

    recode

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// Using import.meta.url to get the current directory
-const dirname = new URL('.', import.meta.url).pathname;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy",
   resolve: {
     alias: {
-      "@": path.resolve(dirname, "./src/"),
-      "@assets": path.resolve(dirname, "./src/assets"),
+      "@": path.resolve(__dirname, "./src/"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
 });
