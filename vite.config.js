a34c68210ed32cae10c19d65069bc1ff3d26fbbc
Thinhncse <cuongthinh5213@gmail.com>
2025-01-16 01:41:48 +0700

    deploy gitpage

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+// Using import.meta.url to get the current directory
+const dirname = new URL('.', import.meta.url).pathname;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-react-deploy",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@": path.resolve(dirname, "./src/"),
+      "@assets": path.resolve(dirname, "./src/assets"),
     },
   },
 });
