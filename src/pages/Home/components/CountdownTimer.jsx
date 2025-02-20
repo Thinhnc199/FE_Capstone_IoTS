f75f74faac6df303d0ea95af31d0eb84b8d79f79
vietlucoder <vietleruoi@gmail.com>
2025-02-20 20:39:40 +0700

    fix ui home page

---
+++ src/pages/Home/components/CountdownTimer.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+const CountdownTimer = () => {
+  // Thời gian đích: 7:00 ngày 01/03/2025
+  const targetDate = new Date("2025-03-01T07:00:00").getTime();
+  const [timeLeft, setTimeLeft] = useState(getTimeRemaining());
+
+  function getTimeRemaining() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    if (distance <= 0) {
+      return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
+
+    return {
+      days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((distance % (1000 * 60)) / 1000),
+    };
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(getTimeRemaining());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center ">
+      {/* Countdown */}
+      <div className="flex space-x-4 text-center">
+        {Object.entries(timeLeft).map(([label, value], index) => (
+          <div key={label} className="flex items-center">
+            {index > 0 && (
+              <span className="text-3xl font-semibold text-red-300">:</span>
+            )}
+            <div>
+              <div className="text-sm font-medium text-gray-700 capitalize">
+                {label}
+              </div>
+              <div className="text-3xl font-semibold text-black">
+                {value.toString().padStart(2, "0")}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Đảm bảo SVGR được thêm vào plugins
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
